eecd8f2c35f4c34898eb59f90dab1eda5d6410ed
shuvro111 <iamshuvrosarkar@gmail.com>
2023-05-19 14:27:19 +0600

    initialize widgetscraft plugin

---
+++ plugin.php
@@ -0,0 +1,151 @@
+<?php
+namespace ElementorHelloWorld;
+
+use ElementorHelloWorld\PageSettings\Page_Settings;
+
+/**
+ * Class Plugin
+ *
+ * Main Plugin class
+ * @since 1.2.0
+ */
+class Plugin {
+
+	/**
+	 * Instance
+	 *
+	 * @since 1.2.0
+	 * @access private
+	 * @static
+	 *
+	 * @var Plugin The single instance of the class.
+	 */
+	private static $_instance = null;
+
+	/**
+	 * Instance
+	 *
+	 * Ensures only one instance of the class is loaded or can be loaded.
+	 *
+	 * @since 1.2.0
+	 * @access public
+	 *
+	 * @return Plugin An instance of the class.
+	 */
+	public static function instance() {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+	/**
+	 * widget_scripts
+	 *
+	 * Load required plugin core files.
+	 *
+	 * @since 1.2.0
+	 * @access public
+	 */
+	public function widget_scripts() {
+		wp_register_script( 'elementor-hello-world', plugins_url( '/assets/js/hello-world.js', __FILE__ ), [ 'jquery' ], false, true );
+	}
+
+	/**
+	 * Editor scripts
+	 *
+	 * Enqueue plugin javascripts integrations for Elementor editor.
+	 *
+	 * @since 1.2.1
+	 * @access public
+	 */
+	public function editor_scripts() {
+		add_filter( 'script_loader_tag', [ $this, 'editor_scripts_as_a_module' ], 10, 2 );
+
+		wp_enqueue_script(
+			'elementor-hello-world-editor',
+			plugins_url( '/assets/js/editor/editor.js', __FILE__ ),
+			[
+				'elementor-editor',
+			],
+			'1.2.1',
+			true
+		);
+	}
+
+	/**
+	 * Force load editor script as a module
+	 *
+	 * @since 1.2.1
+	 *
+	 * @param string $tag
+	 * @param string $handle
+	 *
+	 * @return string
+	 */
+	public function editor_scripts_as_a_module( $tag, $handle ) {
+		if ( 'elementor-hello-world-editor' === $handle ) {
+			$tag = str_replace( '<script', '<script type="module"', $tag );
+		}
+
+		return $tag;
+	}
+
+	/**
+	 * Register Widgets
+	 *
+	 * Register new Elementor widgets.
+	 *
+	 * @since 1.2.0
+	 * @access public
+	 *
+	 * @param Widgets_Manager $widgets_manager Elementor widgets manager.
+	 */
+	public function register_widgets( $widgets_manager ) {
+		// Its is now safe to include Widgets files
+		require_once( __DIR__ . '/widgets/hello-world.php' );
+		require_once( __DIR__ . '/widgets/inline-editing.php' );
+
+		// Register Widgets
+		$widgets_manager->register( new Widgets\Hello_World() );
+		$widgets_manager->register( new Widgets\Inline_Editing() );
+	}
+
+	/**
+	 * Add page settings controls
+	 *
+	 * Register new settings for a document page settings.
+	 *
+	 * @since 1.2.1
+	 * @access private
+	 */
+	private function add_page_settings_controls() {
+		require_once( __DIR__ . '/page-settings/manager.php' );
+		new Page_Settings();
+	}
+
+	/**
+	 *  Plugin class constructor
+	 *
+	 * Register plugin action hooks and filters
+	 *
+	 * @since 1.2.0
+	 * @access public
+	 */
+	public function __construct() {
+
+		// Register widget scripts
+		add_action( 'elementor/frontend/after_register_scripts', [ $this, 'widget_scripts' ] );
+
+		// Register widgets
+		add_action( 'elementor/widgets/register', [ $this, 'register_widgets' ] );
+
+		// Register editor scripts
+		add_action( 'elementor/editor/after_enqueue_scripts', [ $this, 'editor_scripts' ] );
+		
+		$this->add_page_settings_controls();
+	}
+}
+
+// Instantiate Plugin Class
+Plugin::instance();
---
+++ widgets/hello-world.php
@@ -0,0 +1,179 @@
+<?php
+namespace ElementorHelloWorld\Widgets;
+
+use Elementor\Widget_Base;
+use Elementor\Controls_Manager;
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Elementor Hello World
+ *
+ * Elementor widget for hello world.
+ *
+ * @since 1.0.0
+ */
+class Hello_World extends Widget_Base {
+
+	/**
+	 * Retrieve the widget name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget name.
+	 */
+	public function get_name() {
+		return 'hello-world';
+	}
+
+	/**
+	 * Retrieve the widget title.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget title.
+	 */
+	public function get_title() {
+		return __( 'Hello World', 'elementor-hello-world' );
+	}
+
+	/**
+	 * Retrieve the widget icon.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget icon.
+	 */
+	public function get_icon() {
+		return 'eicon-posts-ticker';
+	}
+
+	/**
+	 * Retrieve the list of categories the widget belongs to.
+	 *
+	 * Used to determine where to display the widget in the editor.
+	 *
+	 * Note that currently Elementor supports only one category.
+	 * When multiple categories passed, Elementor uses the first one.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return array Widget categories.
+	 */
+	public function get_categories() {
+		return [ 'general' ];
+	}
+
+	/**
+	 * Retrieve the list of scripts the widget depended on.
+	 *
+	 * Used to set scripts dependencies required to run the widget.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return array Widget scripts dependencies.
+	 */
+	public function get_script_depends() {
+		return [ 'elementor-hello-world' ];
+	}
+
+	/**
+	 * Register the widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function register_controls() {
+		$this->start_controls_section(
+			'section_content',
+			[
+				'label' => __( 'Content', 'elementor-hello-world' ),
+			]
+		);
+
+		$this->add_control(
+			'title',
+			[
+				'label' => __( 'Title', 'elementor-hello-world' ),
+				'type' => Controls_Manager::TEXT,
+			]
+		);
+
+		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'section_style',
+			[
+				'label' => __( 'Style', 'elementor-hello-world' ),
+				'tab' => Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_control(
+			'text_transform',
+			[
+				'label' => __( 'Text Transform', 'elementor-hello-world' ),
+				'type' => Controls_Manager::SELECT,
+				'default' => '',
+				'options' => [
+					'' => __( 'None', 'elementor-hello-world' ),
+					'uppercase' => __( 'UPPERCASE', 'elementor-hello-world' ),
+					'lowercase' => __( 'lowercase', 'elementor-hello-world' ),
+					'capitalize' => __( 'Capitalize', 'elementor-hello-world' ),
+				],
+				'selectors' => [
+					'{{WRAPPER}} .title' => 'text-transform: {{VALUE}};',
+				],
+			]
+		);
+
+		$this->end_controls_section();
+	}
+
+	/**
+	 * Render the widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function render() {
+		$settings = $this->get_settings_for_display();
+
+		echo '<div class="title">';
+		echo $settings['title'];
+		echo '</div>';
+	}
+
+	/**
+	 * Render the widget output in the editor.
+	 *
+	 * Written as a Backbone JavaScript template and used to generate the live preview.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function content_template() {
+		?>
+		<div class="title">
+			{{{ settings.title }}}
+		</div>
+		<?php
+	}
+}
