3c2686426fcd0686cdc3ee04d65c5e72b0aeaeaa
shuvro111 <iamshuvrosarkar@gmail.com>
2023-05-20 15:29:53 +0600

    added css and js for donut chart

---
+++ assets/js/donut-chart.js
@@ -0,0 +1,15 @@
+( function( $ ) {
+	/**
+ 	 * @param $scope The Widget wrapper element as a jQuery element
+	 * @param $ The jQuery alias
+	 */ 
+	var WidgetHelloWorldHandler = function( $scope, $ ) {
+		console.log("Hello World!");
+	};
+	
+	// Make sure you run this code under Elementor.
+	$( window ).on( 'elementor/frontend/init', function() {
+		alert("Hello World!");
+		elementorFrontend.hooks.addAction( 'frontend/element_ready/donut-chart.default', WidgetHelloWorldHandler );
+	} );
+} )( jQuery );
