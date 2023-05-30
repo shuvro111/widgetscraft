58bb86ce72e038095c717d45e1a86034677a5cb6
shuvro111 <iamshuvrosarkar@gmail.com>
2023-05-30 23:44:10 +0600

    modified column chart widget

--- widgets/column-chart.php
+++ widgets/column-chart.php
@@ -14,7 +14,7 @@ if (!defined('ABSPATH'))
  *
  * @since 1.0.0
  */
-class ColumnChart extends Widget_Base
+class Column_Chart extends Widget_Base
 {
 
     public function __construct($data = [], $args = null)
@@ -70,7 +70,7 @@ class ColumnChart extends Widget_Base
      */
     public function get_icon()
     {
-        return 'eicon-posts-ticker';
+        return 'eicon-posts-ticker widgets-craft-widget-panel-icon';
     }
 
     /**
@@ -89,7 +89,7 @@ class ColumnChart extends Widget_Base
      */
     public function get_categories()
     {
-        return ['general'];
+        return ['widgets-craft'];
     }
 
     /**
@@ -123,6 +123,27 @@ class ColumnChart extends Widget_Base
             'section_chart',
             [
                 'label' => 'Chart Settings',
+                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+
+        // X Axis Title
+        $this->add_control(
+            'x_axis_title',
+            [
+                'label' => 'X Axis Title',
+                'type' => \Elementor\Controls_Manager::TEXT,
+                'placeholder' => 'Enter X Axis Title',
+            ]
+        );
+
+        // Y Axis Title
+        $this->add_control(
+            'y_axis_title',
+            [
+                'label' => 'Y Axis Title',
+                'type' => \Elementor\Controls_Manager::TEXT,
+                'placeholder' => 'Enter Y Axis Title',
             ]
         );
 
@@ -183,30 +204,17 @@ class ColumnChart extends Widget_Base
         );
 
 
+        //disable value tooltip
         $this->add_control(
-            'disable_legend_toggle',
+            'disable_value_tooltip',
             [
-                'label' => 'Disable Legend Toggle',
+                'label' => 'Disable Value Tooltip',
                 'type' => \Elementor\Controls_Manager::SWITCHER,
                 'label_on' => 'Yes',
                 'label_off' => 'No',
                 'return_value' => 'yes',
                 'default' => 'no',
-                'description' => 'Toggle the ability for users to click on the legend to hide/show data.',
-            ]
-        );
-
-        //label visibility toggle
-        $this->add_control(
-            'disable_label_toggle',
-            [
-                'label' => 'Disable Label Toggle',
-                'type' => \Elementor\Controls_Manager::SWITCHER,
-                'label_on' => 'Yes',
-                'label_off' => 'No',
-                'return_value' => 'yes',
-                'default' => 'no',
-                'description' => 'Toggle the ability for users to click on the label to hide/show data.',
+                'description' => 'Toggle the ability for users to hover over the chart to see the value on the left.',
             ]
         );
 
@@ -397,6 +405,178 @@ class ColumnChart extends Widget_Base
             ]
         );
 
+        $this->end_controls_section();
+
+
+        //chart styles section
+        $this->start_controls_section(
+            'chart_styles_section',
+            [
+                'label' => 'Chart Styles',
+                'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+            ]
+        );
+
+
+        //chart height
+        $this->add_responsive_control(
+            'chart_height',
+            [
+                'label' => 'Chart Height',
+                'type' => \Elementor\Controls_Manager::SLIDER,
+                'size_units' => [
+                    'px',
+                    '%',
+                    'vh'
+                ],
+                'range' => [
+                    '%' => [
+                        'min' => 0,
+                        'max' => 100,
+                    ],
+                    'px' => [
+                        'min' => 0,
+                        'max' => 1000,
+                    ],
+                    'vh' => [
+                        'min' => 0,
+                        'max' => 100,
+                    ],
+                ],
+                'default' => [
+                    'unit' => 'px',
+                    'size' => 500,
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .chartdiv' => 'height: {{SIZE}}{{UNIT}};',
+                ],
+            ]
+        );
+
+        // Column starting point
+        $this->add_control(
+            'chart_column_start',
+            [
+                'label' => 'Column Starting Point',
+                'description' => 'The starting point of the column. 0.5 means it will start in the middle of the cell.',
+                'type' => \Elementor\Controls_Manager::SLIDER,
+                'size_units' => [
+                    ''
+                ],
+                'range' => [
+                    '' => [
+                        'min' => 0,
+                        'max' => 1,
+                        'step' => 0.1
+                    ]
+                ],
+                'default' => [
+                    'unit' => '',
+                    'size' => 0.1,
+                ],
+
+            ]
+        );
+
+        // Column ending point
+        $this->add_control(
+            'chart_column_width',
+            [
+                'label' => 'Column Width',
+                'description' => 'A value between 0 and 1. 1 means the column will take the whole available width.',
+                'type' => \Elementor\Controls_Manager::SLIDER,
+                'size_units' => [
+                    ''
+                ],
+                'range' => [
+                    '' => [
+                        'min' => 0,
+                        'max' => 1,
+                        'step' => 0.1
+                    ]
+                ],
+                'default' => [
+                    'unit' => '',
+                    'size' => 1,
+                ],
+
+            ]
+        );
+
+        // Grid opacity
+        $this->add_control(
+            'chart_grid_opacity',
+            [
+                'label' => 'Grid Opacity',
+                'description' => 'Grid opacity. Value range is 0 - 1.',
+                'type' => \Elementor\Controls_Manager::SLIDER,
+                'size_units' => [
+                    ''
+                ],
+                'range' => [
+                    '' => [
+                        'min' => 0,
+                        'max' => 1,
+                        'step' => 0.1
+                    ]
+                ],
+                'default' => [
+                    'unit' => '',
+                    'size' => 0.8,
+                ],
+
+            ]
+        );
+
+        // X Axis Label Rotation
+        $this->add_control(
+            'chart_x_axis_label_rotation',
+            [
+                'label' => 'X Axis Label Rotation',
+                'description' => 'X axis label rotation angle. Valid values are from -360 to 360.',
+                'type' => \Elementor\Controls_Manager::SLIDER,
+                'size_units' => [
+                    ''
+                ],
+                'range' => [
+                    '' => [
+                        'min' => -360,
+                        'max' => 360,
+                        'step' => 1
+                    ]
+                ],
+                'default' => [
+                    'unit' => '',
+                    'size' => 0,
+                ],
+
+            ]
+        );
+
+        // Y Axis Label Rotation
+        $this->add_control(
+            'chart_y_axis_label_rotation',
+            [
+                'label' => 'Y Axis Label Rotation',
+                'description' => 'Y axis label rotation angle. Valid values are from -360 to 360.',
+                'type' => \Elementor\Controls_Manager::SLIDER,
+                'size_units' => [
+                    ''
+                ],
+                'range' => [
+                    '' => [
+                        'min' => -360,
+                        'max' => 360,
+                        'step' => 1
+                    ]
+                ],
+                'default' => [
+                    'unit' => '',
+                    'size' => 0,
+                ],
+
+            ]
+        );
 
 
         //typography
@@ -436,23 +616,28 @@ class ColumnChart extends Widget_Base
                     am4core.useTheme(am4themes_animated);
                     var chart = am4core.create("amcharts-chart-<?php echo $this->get_id(); ?>", am4charts.XYChart3D);
                     chart.hiddenState.properties.opacity = 0;
+                    // chart.axis.renderer.grid.template.opacity = 0;
+
+                    // chart.properties.angle = 90;
 
                     chartData.forEach(function (data) {
                         chart.data.push({
                             label: data.label,
                             value: data.value,
-                            color: am4core.color(data.color),
                         });
                     });
 
 
 
-                    console.log(chart.data);
 
                     // Create axes
                     let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+                    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+                    categoryAxis.title.text = '<?php echo $settings['x_axis_title']; ?>';
+                    categoryAxis.title.fontWeight = "bold";
                     categoryAxis.dataFields.category = "label";
-                    categoryAxis.renderer.labels.template.rotation = 270;
+                    categoryAxis.renderer.labels.template.rotation = '<?php echo $settings['chart_x_axis_label_rotation']['size']; ?>';
                     categoryAxis.renderer.labels.template.hideOversized = false;
                     categoryAxis.renderer.minGridDistance = 20;
                     categoryAxis.renderer.labels.template.horizontalCenter = "right";
@@ -461,34 +646,48 @@ class ColumnChart extends Widget_Base
                     categoryAxis.tooltip.label.horizontalCenter = "right";
                     categoryAxis.tooltip.label.verticalCenter = "middle";
 
-                    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-                    valueAxis.title.text = "Income";
+                    // width and column spacing
+                    categoryAxis.renderer.cellEndLocation = '<?php echo $settings['chart_column_width']['size']; ?>';
+                    categoryAxis.renderer.grid.template.strokeOpacity = '<?php echo $settings['chart_grid_opacity']['size']; ?>';
+
+                    valueAxis.renderer.labels.template.rotation = '<?php echo $settings['chart_y_axis_label_rotation']['size']; ?>';
+                    valueAxis.renderer.grid.template.strokeOpacity = '<?php echo $settings['chart_grid_opacity']['size']; ?>';
+
+                    valueAxis.title.text = '<?php echo $settings['y_axis_title']; ?>';
                     valueAxis.title.fontWeight = "bold";
+
+
+                    if ('<?php echo $settings['disable_value_tooltip']; ?>' === 'yes') {
+                        valueAxis.tooltip.disabled = true;
+                    } else {
+                        valueAxis.tooltip.disabled = false;
+                    }
+
+
 
                     // Create series
                     var series = chart.series.push(new am4charts.ColumnSeries3D());
+
+
                     series.dataFields.valueY = "value";
                     series.dataFields.categoryX = "label";
                     series.name = "Visits";
+                    series.name.fontWeight = "bold";
                     series.tooltipText = "{categoryX}: [bold]{valueY}[/]";
-                    series.columns.template.fillOpacity = .8;
+                    series.columns.template.fillOpacity = 0.8;
 
                     var columnTemplate = series.columns.template;
-                    columnTemplate.strokeWidth = 2;
+                    columnTemplate.strokeWidth = 0;
                     columnTemplate.strokeOpacity = 1;
                     columnTemplate.stroke = am4core.color("#FFFFFF");
 
                     columnTemplate.adapter.add("fill", function (fill, target) {
-                        return chart.colors.getIndex(target.dataItem.index);
-                    })
-
-                    columnTemplate.adapter.add("stroke", function (stroke, target) {
-                        return chart.colors.getIndex(target.dataItem.index);
+                        return chartData[target.dataItem.index].color;
                     })
 
                     chart.cursor = new am4charts.XYCursor();
-                    chart.cursor.lineX.strokeOpacity = 0;
-                    chart.cursor.lineY.strokeOpacity = 0;
+                    chart.cursor.lineX.strokeOpacity = 1;
+                    chart.cursor.lineY.strokeOpacity = 1;
                 }
                 loadAmCharts();
             });
