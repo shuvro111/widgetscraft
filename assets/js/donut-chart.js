64dcea5e71d09bbf1569197004a9e4478b809ab1
shuvro111 <iamshuvrosarkar@gmail.com>
2023-06-01 23:31:59 +0600

    modified donut chart js

--- assets/js/donut-chart.js
+++ assets/js/donut-chart.js
@@ -1,14 +1,14 @@
-( function( $ ) {
+(function ($) {
 	/**
- 	 * @param $scope The Widget wrapper element as a jQuery element
+	   * @param $scope The Widget wrapper element as a jQuery element
 	 * @param $ The jQuery alias
-	 */ 
-	var WidgetHelloWorldHandler = function( $scope, $ ) {
-		console.log("Hello World!");
+	 */
+	var WidgetHelloWorldHandler = function ($scope, $) {
+		// console.log("Hello World!");
 	};
-	
+
 	// Make sure you run this code under Elementor.
-	$( window ).on( 'elementor/frontend/init', function() {
-		elementorFrontend.hooks.addAction( 'frontend/element_ready/donut-chart.default', WidgetHelloWorldHandler );
-	} );
-} )( jQuery );
+	$(window).on('elementor/frontend/init', function () {
+		elementorFrontend.hooks.addAction('frontend/element_ready/donut-chart.default', WidgetHelloWorldHandler);
+	});
+})(jQuery);
