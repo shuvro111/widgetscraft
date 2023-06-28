19c345e419fbb06fe201859a8f6465a547ad49c4
shuvro111 <iamshuvrosarkar@gmail.com>
2023-06-28 23:35:08 +0600

    registered widgetscraft category

--- plugin.php
+++ plugin.php
@@ -3,6 +3,13 @@ namespace WidgetsCraft;
 
 use ElementorHelloWorld\PageSettings\Page_Settings;
 
+if (!defined('ABSPATH'))
+	exit; // Exit if accessed directly
+
+
+
+
+
 /**
  * Class Plugin
  *
@@ -42,6 +49,24 @@ class Plugin
 	}
 
 	/**
+	 * Load Editor Styles
+	 * 
+	 * @since 1.2.0
+	 * 
+	 * @access public
+	 *	 
+	 */
+
+	public function editor_styles()
+	{
+		wp_enqueue_style('widgets-craft-editor-styles', plugins_url('assets/css/editor-styles.css', __FILE__));
+	}
+	public function editor_scripts()
+	{
+		wp_enqueue_style('widgets-craft-editor-scripts', plugins_url('assets/css/editor-script.js', __FILE__));
+	}
+
+	/**
 	 * widget_styles
 	 *
 	 * Load required plugin core files.
@@ -52,6 +77,8 @@ class Plugin
 	public function widget_styles()
 	{
 		wp_register_style('donut-chart', plugins_url('/assets/css/donut-chart.css', __FILE__));
+		wp_register_style('advanced-accordion', plugins_url('/assets/css/advanced-accordion.css', __FILE__));
+		wp_register_style('icon-button', plugins_url('/assets/css/icon-button.css', __FILE__));
 	}
 	/**
 	 * widget_scripts
@@ -67,29 +94,7 @@ class Plugin
 		wp_register_script('amcharts-charts', 'https://cdn.amcharts.com/lib/4/charts.js', ['jquery', 'elementor-frontend'], false, true);
 		wp_register_script('amcharts-3d', 'https://cdn.amcharts.com/lib/4/themes/animated.js', ['jquery', 'elementor-frontend'], false, true);
 		wp_register_script('donut-chart', plugins_url('/assets/js/donut-chart.js', __FILE__), ['jquery', 'elementor-frontend'], false, true);
-	}
-
-	/**
-	 * Editor scripts
-	 *
-	 * Enqueue plugin javascripts integrations for Elementor editor.
-	 *
-	 * @since 1.2.1
-	 * @access public
-	 */
-	public function editor_scripts()
-	{
-		add_filter('script_loader_tag', [$this, 'editor_scripts_as_a_module'], 10, 2);
-
-		wp_enqueue_script(
-			'elementor-hello-world-editor',
-			plugins_url('/assets/js/editor/editor.js', __FILE__),
-			[
-				'elementor-editor',
-			],
-			'1.2.1',
-			true
-		);
+		wp_register_script('advanced-accordion', plugins_url('/assets/js/advanced-accordion.js', __FILE__), ['jquery', 'elementor-frontend'], false, true);
 	}
 
 	/**
@@ -112,6 +117,25 @@ class Plugin
 	}
 
 	/**
+	 * Add Category -> WidgetsCraft
+	 *
+	 * Add new category to the Elementor widgets panel.
+	 *
+	 * @since 1.2.0
+	 * @access public
+	 */
+	public function add_widgetscraft_category($elements_manager)
+	{
+		$elements_manager->add_category(
+			'widgets-craft',
+			[
+				'title' => esc_html__('Widgets Craft', 'widgets-craft'),
+				'icon' => 'fas fa-wind',
+			]
+		);
+	}
+
+	/**
 	 * Register Widgets
 	 *
 	 * Register new Elementor widgets.
@@ -126,10 +150,15 @@ class Plugin
 		// Its is now safe to include Widgets files
 		require_once(__DIR__ . '/widgets/donut-chart.php');
 		require_once(__DIR__ . '/widgets/column-chart.php');
+		require_once(__DIR__ . '/widgets/line-chart.php');
+		require_once(__DIR__ . '/widgets/advanced-accordion.php');
+		require_once(__DIR__ . '/widgets/icon-button.php');
 
 		// Register Widgets
 		$widgets_manager->register(new Widgets\Donut_Chart());
-		$widgets_manager->register(new Widgets\ColumnChart());
+		$widgets_manager->register(new Widgets\Column_Chart());
+		$widgets_manager->register(new Widgets\Advanced_Accordion());
+		$widgets_manager->register(new Widgets\Line_Chart());
 	}
 
 	/**
@@ -156,6 +185,9 @@ class Plugin
 	 */
 	public function __construct()
 	{
+		// Register category
+		add_action('elementor/elements/categories_registered', [$this, 'add_widgetscraft_category']);
+
 
 		// Register widget styles
 		add_action('elementor/frontend/after_register_styles', [$this, 'widget_styles']);
@@ -167,6 +199,7 @@ class Plugin
 		add_action('elementor/widgets/register', [$this, 'register_widgets']);
 
 		// Register editor scripts
+		add_action('elementor/editor/after_enqueue_styles', [$this, 'editor_styles']);
 		add_action('elementor/editor/after_enqueue_scripts', [$this, 'editor_scripts']);
 
 		$this->add_page_settings_controls();
